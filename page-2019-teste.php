eeff367cfcfd619bf1566919dc51e06455606b71
Caio <caiolourenco1991@gmail.com>
2019-02-19 23:50:21 -0300

    2019

---
+++ page-2019-teste.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+<div id="main-content">
+
+
+<div id="banner" class="container-fluid">
+	<div class="row chamada">
+		<div class="col-md-6 imagem">
+			<img src="https://www.violeds.com.br/wp-content/uploads/2018/07/mao.jpg" alt="Violeds - O seu maior aliado contra os mosquitos" />
+		</div>
+		<div class="col-md-6 texto">
+			<img src="https://www.violeds.com.br/wp-content/uploads/2018/07/violeds_logo_g.png" alt="" class="header-logo" />
+			<h1> O SEU MAIOR ALIADO <span>CONTRA OS MOSQUITOS</span></h1>
+			<a class="et_pb_button et_pb_more_button et_pb_button_one" href="https://comprar.violeds.com.br/armadilha-eletronica-para-mosquitos">COMPRAR</a>
+		</div>
+	</div>
+</div>
+
+</div>
+<?php get_footer();
