63b1c21da504ca401284bd50f9229768c94b4b9e
Caio <caiolourenco1991@gmail.com>
2019-02-22 20:58:47 -0300

    nada

--- page-2019-teste.php
+++ page-2019-teste.php
@@ -15,5 +15,28 @@
 	</div>
 </div>
 
+<div id="familia" class="container-fluid">
+	<div class="row">
+		<div class="col-md-6">
+			<h1><span>12</span>x mais eficaz!</h1>
+			<h3>MANTENHA SUA <br>FAMÍLIA PROTEGIDA</h3>
+			<p>Enquanto os repelentes evitam o problema, Violeds atrai, captura e mata. Nossa inovadora tecnologia de LED UV combinado ao silencioso sistema de sucção, cria a armadilha perfeita.</p>
+			<p>Violeds elimina 12 vezes mais mosquitos que qualquer outro produto similar ao mercado, inclusive o aedes aegipty, que transmite a dengue, zyka e chikungunya.</p>
+			<p>Tudo isso sem uso de produtos químicos,resíduos ou refis.</p>
+		</div>
+		<div class="col-md-6">
+		</div>
+	</div>
 </div>
+
+<div id="video" class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<iframe src="https://www.youtube.com/embed/fIPYlmqvfB4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+	</div>
+</div>
+
+</div>
+
 <?php get_footer();
