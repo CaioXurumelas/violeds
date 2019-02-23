7268b8602043c8f2ce7a8ba2be81a7e1d139e60a
Caio <caiolourenco1991@gmail.com>
2019-02-22 23:08:19 -0300

    fodase

--- 2019/wp-content/themes/Basic/page.php
+++ 2019/wp-content/themes/Basic/page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The default template for displaying pages.
+ * Template Name: Fullwidth
  *
  * @package Basic
  * @since Basic 1.0
@@ -10,12 +10,39 @@ get_header(); ?>
 
 <main id="main" role="main">
 
-	<?php while(have_posts()): the_post();
+	<?php while(have_posts()) : the_post(); ?>
 
-		// Include the page content template.
-		get_template_part('content', 'page');
+		<div class="container-fluid">
 
-	endwhile; ?>
+			<?php cg_page_title(); ?>
+
+			<div class="content-fullwidth">
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+					<?php the_content(); ?>
+
+					<?php
+						// Author bio.
+						if (is_single() && get_the_author_meta('description')) :
+							get_template_part('author-bio');
+						endif;
+					?>
+
+				</article><!-- #post-## -->
+
+				<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if (comments_open() || get_comments_number()) :
+					comments_template();
+				endif;
+				?>
+
+			</div>
+
+		</div>
+
+	<?php endwhile; ?>
 
 </main>
 
